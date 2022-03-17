05d91ff58048a8cfec7f3a097f22f1bca073e9a8
Julien Hnery <jwh_917@yahoo.com>
2022-03-16 22:57:12 -0400

    Completed Assignment

--- js/index.js
+++ js/index.js
@@ -1,107 +1,125 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+
+document.addEventListener('DOMContentLoaded', (event) => {
     // console.log('DOM fully loaded');
 
     const formSearch = document.getElementById("github-form")
 
-    // console.log(formSearch)
+    formSearch.addEventListener("submit", submitForm)
+    formSearch.addEventListener("submit", renderRepo)
 
-    const searchBar = document.querySelector("input")
-
-    const userList = document.getElementById("user-list") 
-
-    const repoList = document.getElementById("repos-list")   
     
-    let userLogin = document.createElement('li')
-    let userImgLink = document.createElement('li')
-    let userLink = document.createElement('li')
 
-
-
-    formSearch.addEventListener("submit", (event) => {
-        event.preventDefault() 
-        let searchBarValue = searchBar.value
-        // console.log(searchBarValue)
-
-        
-        // after fetch is made make it a function have it called after the form is submitted.
-
-        fetch('https://api.github.com/search/users?q=octocat', {
-            headers: {
-                'Accept' : 'application/vnd.github.v3+json'
-            }})
-        .then(response => response.json())
-        .then(data => 
-            
-            data.items.forEach(element => {
-                // console.log(element)
-                if(element.login === searchBarValue){
-                // console.log(element)
-              
-                userLogin.innerHTML = "Login: " + `${element.login}`
-                userImgLink.innerHTML = "ImgLink: " + `${element.avatar_url}`
-                userLink.innerHTML = "URL: " + `${element.url}`
-
-
-                userList.append(userLogin)
-                userList.append(userImgLink)
-                userList.append(userLink)
-                
-
-                }
-               
-
-
-            })
-            
-        )
-
-
-        fetch('https://api.github.com/users/octocat/repos', {
-            headers: {
-                'Accept' : 'application/vnd.github.v3+json'
-            }})
-        .then(response => response.json())
-        .then(data => 
-            
-            // console.log(data)
-            
-            data.forEach(element => {
-                console.log(element.owner.login)
-                if(element.owner.login === searchBarValue){
-
-
-                let repoLi = document.createElement('li')
-                
-                // console.log(element.name)
-
-
-                repoLi.innerHTML = "Repo Name: " + `${element.name}`
-
-                repoList.append(repoLi)
-                
-                
-            }})
-
-            
-            ) 
-
-
-        // console.log(searchBarValue)
-
-
-    })
-
+    searchBar = document.querySelector("input")
+    repoList = document.getElementById("repos-list") 
 
 
 })
 
+let userName
+
+let repoList 
+
+let searchBar 
+
+let userLogin = document.createElement('li')
+let userImgLink = document.createElement('li')
+let userLink = document.createElement('li')
+let repoLi = document.createElement('li')
 
 
+function submitForm(event){
+    event.preventDefault()
 
-// fetch('https://api.github.com', {
-//     headers: {
-//         'Accept' : 'application/vnd.github.v3+json'
-//     }})
-// .then(response => response.json())
-// .then(data => data) 
+    userLogin.innerHTML = ""
+    userImgLink.innerHTML = ""
+    userLink.innerHTML = ""
     
+    // console.log(event.target.search.value) 
+    // console.log(event.target[0].value) 
+    // console.log(event.target[1].value) 
+    userName = event.target.search.value 
+    
+    // fecth("https://api.github.com/search/users?q=" + userName)
+
+    fetch(`https://api.github.com/search/users?q=${userName}`, 
+    { headers: 
+        {'Accept' : 'application/vnd.github.v3+json'
+    
+    }})
+    .then(response => response.json())
+    .then(data => 
+        
+        renderUser(data)
+        
+        // console.log(data.items) 
+    
+    )
+
+    searchBar.value = ""
+
+    let lis = document.getElementById("repos-list").querySelectorAll("li")
+    for(let i=0; li=lis[i]; i++){
+        li.parentNode.removeChild(li);
+    }
+
+
+}
+
+//renderUser
+
+function renderUser(data){
+    // console.log(userName)
+    // console.log(data.items)
+    
+    const userList = document.getElementById("user-list") 
+    
+    data.items.forEach(element => {
+        // console.log(element)
+        if(element.login === userName){
+            // console.log("GO")
+            userLogin.innerHTML = "Login: " + `${element.login}`
+            userImgLink.innerHTML = "ImgLink: " + `${element.avatar_url}`
+            userLink.innerHTML = "URL: " + `${element.url}`
+            userList.append(userLogin)
+            userList.append(userImgLink)
+            userList.append(userLink)
+            }
+            
+        }
+    )
+
+
+}
+        
+//renderRepo
+function renderRepo(){
+
+    fetch(`https://api.github.com/users/${userName}/repos`, {                               
+    headers: {
+    'Accept' : 'application/vnd.github.v3+json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => 
+            
+        // console.log(data)
+            
+        data.forEach(element => {
+            // console.log(element.owner.login)
+            if(element.owner.login === userName){
+                // console.log(element.owner.login, "=we equal each other=", userName)
+
+                repoLi = document.createElement('li')
+        
+                repoLi.innerHTML = "Repo Name: " + `${element.name}`
+
+                repoList.append(repoLi)
+                
+            }
+
+        })
+
+    )
+   
+}
+ 
